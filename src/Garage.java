7d7689ade655118ae770005d7c235bcd55df5273
Octavio Avila <oavil004@fiu.edu>
2017-09-27 19:59:27 -0400

    Did a lot of work for Garage.java

--- src/Garage.java
+++ src/Garage.java
@@ -3,95 +3,101 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-import java.util.Arrays ;
+import java.util.Arrays;
+
 /**
  *
  * @author Octavio
  */
-public class Garage 
+public class Garage
 {
-    private static final int MAX_SPACE=10;
+
+    private static final int MAX_SPACE = 10;
     private Car[] cars;
     private Car[] carsBeingMoved;
-    private int garageCarCount=0;
-    
+    private int garageCarCount = 0;
+
     public Garage()
     {
-        cars=new Car[MAX_SPACE]; 
+        cars = new Car[MAX_SPACE];
     }
-    
+
     public String arrive(Car hiCar)
-    {   
-        if (garageCarCount==MAX_SPACE)
+    {
+        if (garageCarCount == MAX_SPACE)
         {
             return "Sorry, the garage is full at this moment. Try again later";
         }
-        boolean emptyPositionFound=false;
-        for(int i=0;i<MAX_SPACE&&emptyPositionFound;i++)
+        boolean emptyPositionFound = false;
+        for (int i = 0; i < MAX_SPACE && emptyPositionFound; i++)
         {
-           if (isEmptyAtPosition(i))
-           {
-               cars[i]=hiCar;
-               garageCarCount++;
-               emptyPositionFound=true;
-           }
+            if (isEmptyAtPosition(i))
+            {
+                cars[i] = hiCar;
+                garageCarCount++;
+                emptyPositionFound = true;
+            }
         }
-        
-        
-        return    "JAV001 has arrived at position 10"
+
+        return "JAV001 has arrived at position 10"
                 + "The garage now has 9 available spaces";
     }
-    
+
     public String depart(Car byeCar)
     {
-        boolean carNotFound=true;
-        carsBeingMoved=new Car[MAX_SPACE];
-        for (int i=0; i<MAX_SPACE&&carNotFound;i++)
+        boolean carNotFound = true;
+        carsBeingMoved = new Car[MAX_SPACE];
+        for (int i = 0; i < MAX_SPACE && carNotFound; i++)
         {
-            if (byeCar.getLicensePlate()==cars[i].getLicensePlate())
+            if (byeCar.getLicensePlate() == cars[i].getLicensePlate())
             {
-                carNotFound=false;
-                if (i!=0)
+                carNotFound = false;
+                if (i != 0)
                 {
-                    for (int j=0;j<i;j++)
-                    {
-                        carsBeingMoved[j]=cars[j];
-                        cars[j].increaseMoveCount();
-                    }
-                    
-                    for (int k=i;k<garageCarCount;k++)
-                    {
-                        cars[k]=cars[k+1];
-                    }
+                    moveOutAllCarsBeforeIndex(i);
                 }
                 else
                 {
-                    
+                    for (int l = 0; l < garageCarCount; l++)
+                    {
+                        cars[l] = cars[l + 1];
+                    }
                 }
-                
+                garageCarCount--;
+                return "The car " + byeCar.getLicensePlate() + " has departed. Have a nice day!";
             }
         }
-        
-        if(carNotFound==true)
-        {
-            return "Sorry, that car is not on this garage";
-        }
-        return    "JAV001 has departed from position 10"
-                + "The garage now has 10 available spaces";
+        return "Sorry, that car is not on this garage";
     }
-    
+
     public boolean isEmptyAtPosition(int index)
     {
-        if (cars[index]==null)
+        if (cars[index] == null)
         {
             return true;
         }
-        
         else
         {
             return false;
         }
     }
-    
-    
+
+    public void moveOutAllCarsBeforeIndex(int i)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            carsBeingMoved[j] = cars[j];
+            cars[j].increaseMoveCount();
+        }
+
+        for (int k = i; k < garageCarCount; k++)
+        {
+            cars[k] = cars[k + 1];
+        }
+
+        for (int m = 0; m < i; m++)
+        {
+            cars[m] = carsBeingMoved[m];
+        }
+    }
 }
